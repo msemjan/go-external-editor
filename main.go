e8cbd4c8b634ea7e5bfa9c47572601537f575d5e
ms <68106594+msemjan@users.noreply.github.com>
2023-02-21 19:07:17 +0100

    Added some comments to the main.go

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 	"os/exec"
 )
 
+/*
+ * This is a simple example that shows how to open a temporary file, let the
+ * user edit it, and then output the data into STDOUT.
+ *
+ * In real world, there probably would be some file or data in a database that
+ * is read, then modified by the user, and something meaningful happens with
+ * the input. Alternative usage would be for semi-automated scraping of the
+ * web. The Go program would download some data, the user would check them, and
+ * edit if necessary.
+ */
 func main() {
 	// Create a temporary file
 	f, err1 := ioutil.TempFile("", "tmpfile-")
@@ -16,7 +26,8 @@ func main() {
 	}
 	defer f.Close()
 	defer os.Remove(f.Name())
-
+	
+	// Fill the termporary file with some data
 	f.WriteString("{\n")
 	f.WriteString("\t\"name\": \"\",\n")
 	f.WriteString("\t\"surname\": \"\",")
@@ -30,13 +41,17 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// Open the temporary file in an external editor
 	err2 := cmd.Start()
 	if err2 != nil {
 		log.Printf("2")
 		log.Fatal(err2)
 	}
-
+	
+	// Wait for the user to do their thing...
 	err2 = cmd.Wait()
+
+	// Check if they didn't mess up everything
 	if err2 != nil {
 		log.Printf("Error while editing. Error: %v\n", err2)
 	} else {
@@ -49,5 +64,4 @@ func main() {
 	for fileScanner.Scan() {
 		log.Println(fileScanner.Text())
 	}
-
 }
